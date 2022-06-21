9e55f4528476c24129240052a7b37aef1c914208
Shimizu Amaya <amayashimizu89@gmail.com>
2022-06-21 22:51:38 +0400

    First commit

---
+++ lab1_1/lab1_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число(максимум 5 разрядов):");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            if (n1 > 99999) Console.WriteLine("Не подходит");
+            Console.WriteLine("Введите второе число:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            if (n2 > 99999) Console.WriteLine("Не подходит");
+            Console.WriteLine("Введите третье число:");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            if (n3 > 99999) Console.WriteLine("Не подходит");
+
+            int sum1 = 0;
+            while (n1 != 0 && n1 <= 99999)
+            {
+                //Суммирование цифр числа
+                sum1 += (n1 % 10);
+                n1 /= 10;
+            }
+            int sum2 = 0;
+            while (n2 != 0 && n2 <= 99999)
+            {
+                sum2 += (n2 % 10);
+                n2 /= 10;
+            }
+            int sum3 = 0;
+            while (n3 != 0 && n3 <= 99999)
+            {
+                sum3 += (n3 % 10);
+                n3 /= 10;
+            }
+            if ((sum1 + sum2 + sum3) % (sum1 - sum2) == 0)
+                Console.WriteLine("Делится");
+            else Console.WriteLine("Не делится");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab1_2/lab1_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число меньше 1001:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int chislo = n1;
+            BigInteger n = new BigInteger(chislo);
+            BigInteger rez = new BigInteger(chislo);
+            for (int i = 1; i < n1; i++)
+            {
+                rez = BigInteger.Multiply(rez, n);
+            }
+            Console.WriteLine(n1 + "^" + n1 + "=" + rez);
+
+            uint help;
+
+            int[] counter = new int[10];
+
+            while (!rez.Equals(0))
+            {
+                help = (uint)(BigInteger.Remainder(rez, 10));
+                counter[help] += 1;
+                rez = BigInteger.Divide(rez, 10);
+            }
+            Console.WriteLine("Нулей - " + counter[0] + ", единиц - " + counter[1] + ", двоек - " + counter[2] + ", троек - " + counter[3] +
+                    ", четверок - " + counter[4] + ", пятерок - " + counter[5] + ", шестерок - " + counter[6] + ", семерок - " + counter[7] +
+                    ", восьмерок - " + counter[8] + ", девяток - " + counter[9]);
+            Console.ReadLine();
+        }
+    }
+}
